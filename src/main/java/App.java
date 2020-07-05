835dd92145c33bd2d6ec18c0ca99a32380260d4d
Barsil Nzola <barsiljohn@gmail.com>
2020-07-05 10:50:51 +0300

    changed code to display heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,19 +11,22 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        get("/", (request, response) -> {
+
+        get("/heroes/new", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "index.hbs");
+            return new ModelAndView(model, "newhero-fom.hbs");
         }, new HandlebarsTemplateEngine());
 
-        post("/heroes/new", (request, response) -> {
+        post("/heroes", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             String name = request.queryParams("name");
             int age = Integer.parseInt(request.queryParams("age"));
             String power = request.queryParams("power");
             String weakness = request.queryParams("weakness");
             Hero newHero = new Hero(name, age, power, weakness);
-            return new ModelAndView(model, "success.hbs");
+            model.put("hero", newHero);
+            response.redirect("/");
+            return null;
         }, new HandlebarsTemplateEngine());
 
         get("/", (request, response) -> {
